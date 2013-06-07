c78f4f2cc6403b2091acf37ecc7d7a1d7718eecb
loljimmo12 <spongebob.958@gmail.com>
2013-06-07 21:29:29 +0200

    Screwed around with ImageZooming

--- AMM/src/animemanagermobile/reupload/nl/mangareader/ding.java
+++ AMM/src/animemanagermobile/reupload/nl/mangareader/ding.java
@@ -3,6 +3,8 @@ import android.app.Activity;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.test.TouchUtils;
+import android.text.method.Touch;
 import android.util.FloatMath;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -48,9 +50,6 @@ public class ding extends Activity implements OnTouchListener
         ImageView view = (ImageView) v;
         view.setScaleType(ImageView.ScaleType.MATRIX);
         float scale;
-
-        dumpEvent(event);
-        // Handle touch events here...
 
         switch (event.getAction() & MotionEvent.ACTION_MASK) 
         {
@@ -137,35 +136,5 @@ public class ding extends Activity implements OnTouchListener
         float x = event.getX(0) + event.getX(1);
         float y = event.getY(0) + event.getY(1);
         point.set(x / 2, y / 2);
-    }
-
-    /** Show an event in the LogCat view, for debugging */
-    private void dumpEvent(MotionEvent event) 
-    {
-        String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE","POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?" };
-        StringBuilder sb = new StringBuilder();
-        int action = event.getAction();
-        int actionCode = action & MotionEvent.ACTION_MASK;
-        sb.append("event ACTION_").append(names[actionCode]);
-
-        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP) 
-        {
-            sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
-            sb.append(")");
-        }
-
-        sb.append("[");
-        for (int i = 0; i < event.getPointerCount(); i++) 
-        {
-            sb.append("#").append(i);
-            sb.append("(pid ").append(event.getPointerId(i));
-            sb.append(")=").append((int) event.getX(i));
-            sb.append(",").append((int) event.getY(i));
-            if (i + 1 < event.getPointerCount())
-                sb.append(";");
-        }
-
-        sb.append("]");
-        Log.d("Touch Events ---------", sb.toString());
     }
 }
