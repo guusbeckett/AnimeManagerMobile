00f3a8c85a0c1fc1a1c3d8e550d62a7fa98a6c37
loljimmo12 <spongebob.958@gmail.com>
2013-08-22 14:30:33 +0200

    Added a database helper for the metadata database

---
+++ AMM/src/nl/reupload/animemanagermobile/data/MetadataDatabase.java
@@ -0,0 +1,57 @@
+package nl.reupload.animemanagermobile.data;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MetadataDatabase extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "AMMMeta.db";
+
+    public MetadataDatabase(Context context) {
+    	super(context, getPath(context), null, DATABASE_VERSION);
+    }
+    
+    public static String getPath(Context context) {
+    	SharedPreferences settings = context.getSharedPreferences("AMMprefs", 0);
+        if (settings.getBoolean("MetadataExtStorage", false))
+        	return context.getExternalFilesDir(null).getAbsolutePath() + "/" + DATABASE_NAME;
+        else
+        	return DATABASE_NAME;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS MetaData (" +
+	        		"Type INTEGER, " +
+	        		"Source INTEGER, " +
+	        		"ID INTEGER, " +
+	        		"SeriesType TEXT, " +
+	        		"EPorCHcnt INTEGER, " +
+	        		"StartDate INTEGER" +
+	        		"EndDate INTEGER" +
+	        		"Titles TEXT" +
+	        		"Related TEXT" +
+	        		"Similar TEXT" +
+	        		"Reccomendations TEXT" +
+	        		"URL TEXT" +
+	        		"Creators TEXT" +
+	        		"Description TEXT" +
+	        		"Ratings TEXT" +
+	        		"Picture TEXT" +
+	        		"Categories TEXT" +
+	        		"Resources TEXT" +
+	        		"Tags TEXT" +
+	        		"Characters TEXT" +
+	        		"EPsorCHs TEXT" +
+	        		");");
+    }
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+}
