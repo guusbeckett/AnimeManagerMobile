5744d171bd32e8531559a713d76b27788a9a678c
loljimmo12 <spongebob.958@gmail.com>
2013-09-09 00:03:46 +0000

    Revert "Added licence information"
    This reverts commit ec870e44418f415fcae7ff65ff1134d2caaf1edf.

--- AMM/src/nl/reupload/animemanagermobile/storages/MyAnimeListFS.java
+++ AMM/src/nl/reupload/animemanagermobile/storages/MyAnimeListFS.java
@@ -1,16 +1,3 @@
-AnimeManagerMobile is an android app developed to make the organisation of animelist easier to do on mobile devices Copyright (C) 2013 Jim van Abkoude
-
-
-
-This program is free software; you can redistribute it and/or modify it under the terms of the Reupload Open Source Licence as punlished by Reupload; either version 0.1 of the License, or (at your option) any later version.
-
-
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-
-
-You should have received a copy of the Reupload Open Source Licence along with this library; if not, contact the distrubutor of this program
 package nl.reupload.animemanagermobile.storages;
 
 import nl.reupload.animemanagermobile.data.MALWrapper;
